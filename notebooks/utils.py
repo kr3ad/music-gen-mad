fb1573ba1a26a7229f6a388e1c9b57bebfdfbadd
Kathleen Ross <kross1@zora.itcs.ccny.lan>
2026-09-01 14:25:03 -0400

    investigating cos sim and ablating codebooks

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -67,7 +67,6 @@ def extract_attentions(model, inputs_dict, chunk_size=None):
         for path, inputs in inputs_dict.items():
             audio_features = inputs["input_values"]
             decoder_input_ids = model.audio_encoder.encode(audio_features).audio_codes
-            print(decoder_input_ids.shape)
             decoder_outputs = model.decoder(
                 input_ids=decoder_input_ids,
                 encoder_hidden_states=None,
